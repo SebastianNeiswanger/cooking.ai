2f05172e662ba794281f569391b0a0148fe82d54
Brandon <brandonbarker169@gmail.com>
2023-04-26 23:36:18 -0400

    Initial training changes

--- Assets/Scripts/PlayerAgent.cs
+++ Assets/Scripts/PlayerAgent.cs
@@ -38,7 +38,7 @@ public class PlayerAgent : Agent
     {
         intr = interactionObj.GetComponent<Interact>();
         cc = GetComponent<CharacterCtrl>();
-        rewardGroup = System.Convert.ToInt32(Academy.Instance.EnvironmentParameters.GetWithDefault("rewardGroup", defaultRewardGroup));
+        rewardGroup = System.Convert.ToInt32(Academy.Instance.EnvironmentParameters.GetWithDefault("lessonNum", defaultRewardGroup));
         setRewards(rewardGroup);
         SetReward(0f);
     }
@@ -241,7 +241,7 @@ public class PlayerAgent : Agent
         transform.position = new Vector3(2.0f, -0.49f, 2.0f);
 
         // Set reward group
-        rewardGroup = System.Convert.ToInt32(Academy.Instance.EnvironmentParameters.GetWithDefault("rewardGroup", defaultRewardGroup));
+        rewardGroup = System.Convert.ToInt32(Academy.Instance.EnvironmentParameters.GetWithDefault("lessonNum", defaultRewardGroup));
 
         // Get new kitchen (once the single kitchen Proof of Concept works)
             // kitchen.filepath = random kitchen path
